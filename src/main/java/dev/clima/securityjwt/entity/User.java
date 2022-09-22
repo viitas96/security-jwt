25f356166109f09ce6e82e3786773b79f50c7c32
Clima Victor <climav96@gmail.com>
2022-09-22 15:55:56 +0300

    added user entity

---
+++ src/main/java/dev/clima/securityjwt/entity/User.java
@@ -0,0 +1,32 @@
+package dev.clima.securityjwt.entity;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import static com.fasterxml.jackson.annotation.JsonProperty.Access.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String email;
+
+    @JsonProperty(access = WRITE_ONLY)
+    private String password;
+
+}
