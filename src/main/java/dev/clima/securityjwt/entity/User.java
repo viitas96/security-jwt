a818203d7c0378fd93a297a3bf9709ab039e8a63
Clima Victor <climav96@gmail.com>
2022-09-22 16:01:55 +0300

    added repository and added unique constraint

--- src/main/java/dev/clima/securityjwt/entity/User.java
+++ src/main/java/dev/clima/securityjwt/entity/User.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -24,6 +25,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @Column(unique = true)
     private String email;
 
     @JsonProperty(access = WRITE_ONLY)
