34eeccec8350fa5ea7a01ea7f717d17feb43742d
Clima Victor <climav96@gmail.com>
2022-09-23 09:39:42 +0300

    added UserDTO

---
+++ src/main/java/dev/clima/securityjwt/dto/UserDTO.java
@@ -0,0 +1,20 @@
+package dev.clima.securityjwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private String email;
+
+    private String password;
+
+}
